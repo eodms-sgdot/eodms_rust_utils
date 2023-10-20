40d3529854d5d4f7d28cf42d3300083af3b1089f
kdesjard <kristian.desjardins@nrcan-rncan.gc.ca>
2023-10-20 11:25:40 +0000

    initial commit of a dropbox module to monitor directory+file based dropboxes

---
+++ src/dropbox.rs
@@ -0,0 +1,172 @@
+use std::{
+	path::PathBuf,
+	fs,
+	io::Error as IOError,
+};
+use log::{debug,info,error};
+use futures::future::BoxFuture;
+use regex::Regex;
+use tokio::time::{Duration,sleep};
+
+use crate::file::{DirError,directory_exists};
+use crate::signal::{Signal,SignalType};
+
+const DEFAULT_SLEEP_INTERVAL: u64 = 5;
+
+pub struct DropBoxDirs<'a> {
+	pub target: &'a str,
+	pub error: &'a str,
+	pub processing: &'a str,
+	pub processed: &'a str,
+}
+
+#[derive(Clone)]
+pub struct DropBoxes {
+	pub target: PathBuf,
+	pub error: PathBuf,
+	pub processing: PathBuf,
+	pub processed: PathBuf,
+}
+
+type HandlerFn = Box<dyn Fn(DropBoxes,Vec<String>) -> BoxFuture<'static,Result<(),DropBoxError>> + Send + Sync>;
+
+pub struct DropBox {
+	dropboxes: DropBoxes,
+	target_filter: Option<Regex>,
+	handler: HandlerFn,
+	pub signal: Signal,
+}
+
+pub enum DropBoxDir {
+	Target,
+	Error,
+	Processing,
+	Processed,
+}
+
+#[derive(Debug)]
+pub enum DropBoxError {
+	Path(DirError),
+	Regex(regex::Error),
+	IO(IOError),
+}
+
+impl From<regex::Error> for DropBoxError {
+	fn from(e: regex::Error) -> DropBoxError {
+		DropBoxError::Regex(e)
+	}
+}
+
+impl From<DirError> for DropBoxError {
+	fn from(e: DirError) -> DropBoxError {
+		DropBoxError::Path(e)
+	}
+}
+
+impl From<IOError> for DropBoxError {
+	fn from(e: IOError) -> DropBoxError {
+		DropBoxError::IO(e)
+	}
+}
+
+impl DropBox {
+	pub fn new(
+				dirs:    DropBoxDirs<'_>,
+				rxstr:   Option<String>,
+				handler: HandlerFn,
+			) -> Result<DropBox,DropBoxError> {
+
+		let rx = match rxstr {
+			Some(s) => Some(Regex::new(&s)?),
+			None    => None,
+		};
+		let signal = Signal::new();
+		Ok(DropBox {
+			dropboxes: DropBoxes {
+				target: directory_exists(dirs.target)?,
+				error: directory_exists(dirs.error)?,
+				processing: directory_exists(dirs.processing)?,
+				processed: directory_exists(dirs.processed)?,
+			},
+			target_filter: rx,
+			handler,
+			signal,
+		})
+	}
+	pub fn list(&self, dirtype: DropBoxDir) -> Result<Vec<String>,DropBoxError> {
+		let dir = match dirtype {
+			DropBoxDir::Target => &self.dropboxes.target,
+			DropBoxDir::Error => &self.dropboxes.error,
+			DropBoxDir::Processing => &self.dropboxes.processing,
+			DropBoxDir::Processed => &self.dropboxes.processed,
+		};
+		let mut files = vec![];
+		for entry in fs::read_dir(dir)? {
+			let entry = entry?;
+			let path = entry.path();
+			if path.is_file() {
+				if let Some(osstr) = path.file_name() {
+					if let Some(str) = osstr.to_str() {
+						let mut dirstr = dir.to_str().unwrap().to_string();
+						dirstr.push('/');
+						dirstr.push_str(str);
+						if let Some(r) = &self.target_filter {
+							if r.is_match(str) {
+								files.push(dirstr);
+							}
+						} else {
+							files.push(dirstr);
+						}
+					}
+				}
+			}
+		}
+		Ok(files)
+	}
+
+	pub async fn monitor(&self, sleep_interval: Option<u64>) -> Result<(),DropBoxError> {
+		let mut signal_receiver = self.signal.subscribe();
+		let sleep_time = if let Some(s) = sleep_interval {
+			Duration::from_secs(s)
+		} else {
+			Duration::from_secs(DEFAULT_SLEEP_INTERVAL)
+		};
+		info!("Dropbox monitor sleeping {} seconds for every iteration",sleep_time.as_secs());
+		loop {
+			tokio::select! {
+				signal = signal_receiver.recv() => {
+					match signal {
+						Ok(s) => {
+							match s {
+								SignalType::Shutdown => {
+									info!("Received shutdown signal, quitting");
+									return Ok(());
+								},
+								_ => {
+								},
+							}
+						},
+						Err(e) => {
+							error!("signal: {}",e);
+						}
+					}
+				},
+			};
+			let files = self.list(DropBoxDir::Target)?;
+			if !files.is_empty() {
+				info!("Processing {} files",files.len());
+				debug!("Files:\n{:#?}",files);
+				match (self.handler)(self.dropboxes.clone(),files).await {
+					// need a way to break the loop for fatal errors!!
+					Ok(()) => {},
+					Err(e) => {
+						error!("{:?}",e);
+						break Err(e);
+					}
+				}
+			}
+			sleep(sleep_time).await;
+		}
+	}
+}
+
